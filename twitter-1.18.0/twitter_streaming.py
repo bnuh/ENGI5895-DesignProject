259e7f19a5456d71194a0db24dec59ac5669a50e
bnuh <bryan.melanson@gmail.com>
2018-02-15 21:15:52 -0330

    Initial commit

---
+++ twitter-1.18.0/twitter_streaming.py
@@ -0,0 +1,42 @@
+# Import the necessary package to process data in JSON format
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Import the necessary methods from "twitter" library
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = '1942242924-K1pi57z1SljvWDib3Y9XgIEYcgK6uTQy7jiLHMd'
+ACCESS_SECRET = 'aIZCXgTnQqKWMTaYKcMSkm7Cf5yqZw5w0GqPhvzm7mj9L'
+CONSUMER_KEY = '20yNRIOurzQaKzs9t7C4HXWuV'
+CONSUMER_SECRET = 'Pl9z3CqDrj5QSBYkMDaKxT6cx4AUSlf4Jm7UOf8ovkdgcuQvcD'
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+# Initiate the connection to Twitter Streaming API
+#twitter_stream = TwitterStream(auth=oauth)
+twitter_userstream = TwitterStream(auth=oauth, domain='userstream.twitter.com')
+
+# Get a sample of the public data following through Twitter
+#iterator = twitter_stream.statuses.sample()
+iterator = twitter_userstream.statuses.sample()
+#iterator = twitter_stream.statuses.filter(track="Trump", language="en")
+# Print each tweet in the stream to the screen 
+# Here we set it to stop after getting 1000 tweets. 
+# You don't have to set it to stop, but can continue running 
+# the Twitter API to collect data for days or even longer. 
+tweet_count = 1000
+for tweet in iterator:
+    tweet_count -= 1
+    # Twitter Python Tool wraps the data returned by Twitter 
+    # as a TwitterDictResponse object.
+    # We convert it back to the JSON format to print/score
+    #print json.dumps(tweet)  
+    
+    # The command below will do pretty printing for JSON data, try it out
+    print json.dumps(tweet, indent=4)
+       
+    if tweet_count <= 0:
+        break 
