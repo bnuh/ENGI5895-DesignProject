0b4954fcc5b63a0c66cfd7f597e98024b914d27d
bnuh <bryan.melanson@gmail.com>
2018-02-25 12:43:46 -0330

    IntelliJ Java ProjecT

---
+++ ENGI5895-IntelliJ/src/Main.java
@@ -0,0 +1,30 @@
+package swsmr;
+import java.util.*;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class Main {
+
+    public static void main(String[] args) throws TwitterException {
+
+            ConfigurationBuilder cf = new ConfigurationBuilder();
+
+            cf.setDebugEnabled(true)
+                    .setOAuthConsumerKey("20yNRIOurzQaKzs9t7C4HXWuV")
+                    .setOAuthConsumerSecret("Pl9z3CqDrj5QSBYkMDaKxT6cx4AUSlf4Jm7UOf8ovkdgcuQvcD")
+                    .setOAuthAccessToken("1942242924-aQgcIo4phlvOu38IGOphtIazT3mSiWUAnkTypHX")
+                    .setOAuthAccessTokenSecret("zTY5tAJKVG6aGW44DIEHzprxBDXZzpxVQfA44dcjl6KSt");
+
+            TwitterFactory tf = new TwitterFactory(cf.build());
+            twitter4j.Twitter twitter = tf.getInstance();
+
+            List<Status> status = twitter.getHomeTimeline();
+            for (Status st : status){
+                System.out.println(st.getUser().getName()+ "\n" + st.getText());
+
+            }
+    }
+
+}
