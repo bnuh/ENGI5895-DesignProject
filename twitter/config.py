41fd0419ba40ff96b8913e8070102034ee2d4b5e
bnuh <bryan.melanson@gmail.com>
2018-02-26 18:31:05 -0330

    cleaning directory

--- twitter/config.py
+++
@@ -1,5 +0,0 @@
-# Variables that contains the user credentials to access Twitter API
-consumer_key = '20yNRIOurzQaKzs9t7C4HXWuV'
-consumer_secret = 'Pl9z3CqDrj5QSBYkMDaKxT6cx4AUSlf4Jm7UOf8ovkdgcuQvcD'
-access_key = '1942242924-aQgcIo4phlvOu38IGOphtIazT3mSiWUAnkTypHX'
-access_secret = 'zTY5tAJKVG6aGW44DIEHzprxBDXZzpxVQfA44dcjl6KSt'
