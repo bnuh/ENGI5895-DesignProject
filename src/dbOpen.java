645cb488cb8840c89fa7a0d7b6756d29f1e0e89f
bnuh <bryan.melanson@gmail.com>
2018-02-26 21:25:15 -0330

    Database functions

--- src/dbOpen.java
+++ src/dbOpen.java
@@ -7,7 +7,7 @@ public class dbOpen {
 
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            c = DriverManager.getConnection("jdbc:sqlite:swsmr.db");
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
--- src/dbUpdate.java
+++ src/dbUpdate.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 
-public class dbCreate {
+public class dbUpdate {
 
     public static void main( String args[] ) {
         Connection c = null;
@@ -8,23 +8,22 @@ public class dbCreate {
 
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:swsmr.db");
+            c = DriverManager.getConnection("jdbc:sqlite:./out/swsmr.db");
+            c.setAutoCommit(false);
             System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
-            String sql = "CREATE TABLE TWEETS " +
-                    "(ID INT PRIMARY KEY     NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " LOCATION            CHAR(100)     NOT NULL, " +
-                    " CONTENT       TEXT, " +
-                    " DATECREATED         CHAR(12))";
+            String sql = "INSERT INTO TWEETS (ID,NAME,LOCATION,CONTENT,DATECREATED) " +
+                    "VALUES (1, 'Paul', 'Mount Pearl', 'Sup wit it', 'Feb 26, 2018' );";
             stmt.executeUpdate(sql);
+
             stmt.close();
+            c.commit();
             c.close();
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
-        System.out.println("Table created successfully");
+        System.out.println("Records created successfully");
     }
 }
